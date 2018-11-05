56db8ff6068cd2f23c7548e7b4e7c2d2c8153283
Logan Garber <Logangarber@hotmail.com>
2018-11-05 16:56:10 -0500

    added DBHelper class

--- app/src/main/java/marshmallowboom/com/helpimhungry/PantryActivity.java
+++ app/src/main/java/marshmallowboom/com/helpimhungry/PantryActivity.java
@@ -28,7 +28,7 @@ public class PantryActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Should this exist?", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
