d4ee2f5a8b665cf999981039ae481805d84f0d08
Logan Garber <Logangarber@hotmail.com>
2018-11-27 17:25:20 -0500

    added dark theme and created blank recipe display activity

---
+++ app/src/main/java/marshmallowboom/com/helpimhungry/RecipeActivity.java
@@ -0,0 +1,13 @@
+package marshmallowboom.com.helpimhungry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
